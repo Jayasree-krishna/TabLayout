f9e1742283ddcc0e75050dabf664f71e5ce64903
Jayasree-krishna <krishnasarasamma1996@gmail.com>
2020-05-12 08:53:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tablayout/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.tablayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableRow;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout=(TabLayout)findViewById(R.id.mytab);
+        viewPager=(ViewPager)findViewById(R.id.myview_page);
+        viewPager.setAdapter(new mypageadapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    class mypageadapter extends FragmentPagerAdapter{
+
+        String data[]={"calls","status","video"};
+
+        public mypageadapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            if(position==0){
+                return new CallList();
+            }
+            if(position==1)
+            {
+                return new status_list();
+            }
+            if(position==3)
+            {
+                return new VideoList();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return data.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return data[position];
+        }
+    }
+
+}
+
